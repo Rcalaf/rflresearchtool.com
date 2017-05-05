e2ec7b5289e3a14bd5641dc74268f23ef126aba4
Roger <rcalaf.cot@gmail.com>
2017-05-05 18:06:51 +0200

    added basic html auth

--- config/initializers/Instagram.rb
+++ config/initializers/Instagram.rb
@@ -1,6 +1,8 @@
 Instagram.configure do |config|
   config.client_id = "87fff0273d204567a46216b39c0a6ff6"
   config.client_secret = "98192e9bb51f4fa7ad3e62ec07c37644"
+  #config.client_id = "7f290f28d9e746eb90515170fcb24b71"
+  #config.client_secret = "1c8373b0521d44109e2ba6ad9660e940"
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
